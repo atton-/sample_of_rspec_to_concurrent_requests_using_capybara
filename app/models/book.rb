643836c3653383f6276e54341304a70541a76281
atton <e115763@gmail.com>
2015-08-01 09:32:24 +0900

    Assign Isbn of Book automatically

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,13 @@
 class Book < ActiveRecord::Base
+  LimitOfISBN = 1000
+
+  validates_presence_of   :name, :isbn, :price
+  validates_uniqueness_of :isbn
+
+  before_validation do
+    candidate = LimitOfISBN.times.find do |isbn|
+      not Book.exists?(isbn: isbn)
+    end
+    self.isbn = candidate
+  end
 end
