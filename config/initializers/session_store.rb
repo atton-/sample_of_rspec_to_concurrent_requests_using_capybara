2ac818b2de64d549065785fd16d0a1226ada1a27
atton <e115763@gmail.com>
2015-08-01 09:16:01 +0900

    rails new sample_of_rspec_to_concurrent_requests_using_capybara

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.session_store :cookie_store, key: '_sample_of_rspec_to_concurrent_requests_using_capybara_session'
